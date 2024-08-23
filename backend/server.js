895118a115350a422a7395b824beae75646aee14
sro2005 <santiagor.o06105@gmail.com>
2024-08-22 19:21:40 -0500

    Inicializar proyecto de React

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://company-home-appliances-sro.vercel.app'  // Permitir solo este dominio
+}));
 app.use(express.json());
 
 // Conexión a MongoDB
