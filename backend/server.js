d1526567cc24f59214c031cee143bd0f632f0e3c
sro2005 <santiagor.o06105@gmail.com>
2024-09-01 23:02:42 -0500

    Inicializar proyecto de React

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,12 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Configurar las opciones CORS
-const corsOptions = {
+// Define los orígenes permitidos
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+
+// Configuración del middleware CORS
+app.use(cors({
   origin: (origin, callback) => {
-    const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
     if (allowedOrigins.includes(origin) || !origin) {
       callback(null, true);
     } else {
@@ -22,10 +24,8 @@ const corsOptions = {
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
-};
+}));
 
-// Middleware
-app.use(cors(corsOptions));
 
 app.use(helmet()); // Mejorar la seguridad HTTP
 app.use(express.json());
