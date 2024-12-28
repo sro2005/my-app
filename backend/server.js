2f17adfe6a67c619514f0fad7cb331ff75ddabee
sro2005 <santiagor.o06105@gmail.com>
2024-12-27 21:41:27 -0500

    Inicializar proyecto de React

--- backend/server.js
+++ backend/server.js
@@ -34,10 +34,7 @@ app.use(express.json());
 
 // Conexión a MongoDB
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => {
     console.error('Failed to connect to MongoDB', err);
