aaa23572664de0fe948541612a10c63fa1c06a11
sro2005 <santiagor.o06105@gmail.com>
2025-02-09 17:27:21 -0500

    Inicializar & Actualizar proyecto de React

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,8 +22,7 @@ const Header = () => {
       <div className="separator-shadow"></div>
       <h4>• TRANSFORMA TU HOGAR CON LA ENERGÍA DEL FUTURO •</h4>
       <nav aria-label="Main Navigation">
-        <ul>
-          {user ? (
+        {user ? (
             <div className="nav-container">
               <ul className="nav-links">
                 <li>
@@ -33,33 +32,33 @@ const Header = () => {
                   </Link>
                 </li>
 
-                {/* Solo mostrar "Agregar Producto" si el rol es admin */}
-                {user?.role === 'admin' && (
-                  <li>
-                    <Link to="/producto-form">
-                      <div className="nav-icon"><FontAwesomeIcon icon={faPlusCircle} /></div>
-                      <div className="nav-text">AGREGAR PRODUCTOS</div>
-                    </Link>
-                  </li>
-                )}
-                {/* Solo mostrar "Realizar Pedidos" si el rol es user */}
-                {user?.role === 'user' && (
+              {/* Solo mostrar "Agregar Producto" si el rol es admin */}
+              {user?.role === 'admin' && (
+                <li>
+                  <Link to="/producto-form">
+                    <div className="nav-icon"><FontAwesomeIcon icon={faPlusCircle} /></div>
+                    <div className="nav-text">AGREGAR PRODUCTOS</div>
+                  </Link>
+                </li>
+              )}
+              {/* Solo mostrar "Realizar Pedidos" si el rol es user */}
+              {user?.role === 'user' && (
                 <li>
                   <Link to="/pedido-form">
                     <div className="nav-icon"><FontAwesomeIcon icon={faCheckSquare} /></div>
                     <div className="nav-text">REALIZAR PEDIDOS</div>
                   </Link>
                 </li>
-                )}
-                {/* Solo mostrar "Gestión de Clientes" si el rol es admin */}
-                {user?.role === 'admin' && (
-                  <li>
-                    <Link to="/listado-clientes">
-                      <div className="nav-icon"><FontAwesomeIcon icon={faUsers} /></div>
-                      <div className="nav-text">GESTIÓN DE CLIENTES</div>
-                    </Link>
-                  </li>
-                )}
+              )}
+              {/* Solo mostrar "Gestión de Clientes" si el rol es admin */}
+              {user?.role === 'admin' && (
+                <li>
+                  <Link to="/listado-clientes">
+                    <div className="nav-icon"><FontAwesomeIcon icon={faUsers} /></div>
+                    <div className="nav-text">GESTIÓN DE CLIENTES</div>
+                  </Link>
+                </li>
+              )}
 
                 <li>
                   <Link to="/listado-productos">
@@ -72,28 +71,30 @@ const Header = () => {
                   <Link to="/listado-pedidos">
                     <div className="nav-icon"><FontAwesomeIcon icon={faClipboardList} /></div>
                     <div className="nav-text">
-                {user?.role === 'admin' ? 'GESTIÓN DE PEDIDOS' : 'MIS ÓRDENES'}
+              {user?.role === 'admin' ? 'GESTIÓN DE PEDIDOS' : 'MIS ÓRDENES'}
                     </div>
                   </Link>
                 </li>
                 
-                {/* Solo mostrar "Perfil" si el rol es user */}
-                {user?.role === 'user' && (
-                  <li>
+              {/* Solo mostrar "Perfil" si el rol es user */}
+              {user?.role === 'user' && (
+                <li>
                   <Link to="/perfil-cliente">
-                 <div className="nav-icon"><FontAwesomeIcon icon={faUser} /></div>
-                 <div className="nav-text">PERFIL</div>
+                    <div className="nav-icon"><FontAwesomeIcon icon={faUser} /></div>
+                    <div className="nav-text">PERFIL</div>
                   </Link>
-               </li>
-                )}
+                </li>
+              )}
+             
+                <li className="logout-item">
+                  <button className="logout-button" onClick={logout}>
+                    <div className="nav-icon"><FontAwesomeIcon icon={faSignOutAlt} /></div>
+                    <div className="nav-text">SALIR</div>
+                  </button>
+                </li>
               </ul>
-              <div className="nav-actions">
-                <button className="logout-button" onClick={logout}>
-                  <FontAwesomeIcon icon={faSignOutAlt} />
-                </button>
-              </div>
             </div>
-          ) : (
+        ) : (
             <div className="nav-container">
               <ul className="nav-links">
                 <li>
@@ -110,8 +111,7 @@ const Header = () => {
                 </li>
               </ul>
             </div>
-          )}
-        </ul>
+        )}
       </nav>
       {showGoodbyeMessage && (
         <div className="goodbye-message">
@@ -123,4 +123,3 @@ const Header = () => {
 };
 
 export default Header;
-
